7120e34a83e15062f1302666ef47a4eab5345512
19yusufozcan <19yusufozcan@gmail.com>
2026-01-26 20:59:29 -0500

    added try catch for the Custemer Class first time.

--- Flight_Management/Customers.cs
+++ Flight_Management/Customers.cs
@@ -1,8 +1,10 @@
-﻿namespace Flight_Management;
+﻿using System.Text.RegularExpressions;
+
+namespace Flight_Management;
 
 public class Customers
 {
-    
+
     public string customerName;
     public string customerEmail;
     public long customerPhone;
@@ -15,140 +17,168 @@ public class Customers
         this.customerPhone = customerPhone;
         this.customer_id = customer_id;
     }
-    
+
     public string summary()
     {
-        return $" ID: {customer_id} ,Name: {customerName}, Email: {customerEmail}, Phone: {customerPhone},";
+        return $"ID: {customer_id}, Name: {customerName}, Email: {customerEmail}, Phone: {customerPhone}";
     }
 
     public string removedCustumers()
     {
-        return $" Custumer Named {customerName} - ID: {customer_id} Successfully  removed";
+        return $"Customer Named {customerName} - ID: {customer_id} Successfully removed";
     }
 
-    public static void getCustomerMenu(List<Customers> customer )
+    public static void getCustomerMenu(List<Customers> customer)
     {
-        
-        bool backToMainMenu =  true;
+
+        bool backToMainMenu = true;
         while (backToMainMenu)
+        {
+            Console.WriteLine("1 - Add Customer");
+            Console.WriteLine("2 - View Customers");
+            Console.WriteLine("3 - Delete Customers");
+            Console.WriteLine("4 - Back to Main Menu");
+            
+            int userInput;
+            while (true)
+            {
+                try
+                {
+                    userInput = Int32.Parse(Console.ReadLine());
+                    Console.Clear();
+                    break;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Please select a valid number on the menu!");
+                }
+            }
+
+            switch (userInput)
+            {
+                case 1:
+                    Console.WriteLine("Enter Your Name:");
+                    string customerName = Console.ReadLine();
+                    Console.Clear();
+                    Console.WriteLine("Enter Your Email:");
+                    string customerEmail = Console.ReadLine();
+                    Console.Clear();
+                    Console.WriteLine("Enter Your Phone:");
+
+                    long customerPhone;
+                    while (true)
                     {
-                        Console.WriteLine("1 - Add Customer");
-                        Console.WriteLine("2 - View Customers");
-                        Console.WriteLine("3 - Delete Customers");
-                        Console.WriteLine("4 - Back to Main Menu");
-
-                        int userInput2 = Int32.Parse(Console.ReadLine());
-
-                        switch (userInput2)
+                        try
                         {
-                            case 1:
-                                Console.WriteLine("1 - Add Customer");
-                                Console.Clear();
-                                Console.WriteLine("Enter Your Name:");
-                                string customerName = Console.ReadLine();
-                                Console.Clear();
-                                Console.WriteLine("Enter Your Email:");
-                                string customerEmail = Console.ReadLine();
-                                Console.Clear();
-                                Console.WriteLine("Enter Your Phone:");
-                                long customerPhone = Int64.Parse(Console.ReadLine());
-                                Console.Clear();
-
-                                Random rndm = new Random();
-                                int customer_id = rndm.Next(1000, 9999);
-
-                                bool isSameId = true;
-
-                                foreach (var c in customer)
-                                {
-                                    if (c.customer_id == customer_id)
-                                    {
-                                        isSameId = true;
-                                        break;
-                                    }
-                                }
-
-                                if (isSameId)
-                                {
-                                    
-                                }
-                                else
-                                {
-                                    Customers newCustomer = new Customers(customerName, customerEmail, customerPhone, customer_id);
-                                    customer.Add(newCustomer);
-                                }
-
-                                bool isSameCustomer = false;
-
-                                foreach (var customers in customer)
-                                {
-                                    if (customers.customerEmail == customerEmail || customers.customerPhone == customerPhone)
-                                    {
-                                        isSameCustomer = true;
-                                        break;
-                                    }
-                                }
-
-                                if (isSameCustomer)
-                                {
-                                    Console.WriteLine(
-                                        $"This E-Mail ({customerEmail}) or Phone Number ({customerPhone}) already in the system!");
-                                }
-                                else
-                                {
-                                    Customers newCustomer = new Customers(customerName, customerEmail, customerPhone, customer_id);
-                                    customer.Add(newCustomer);
-                                    Console.WriteLine("Successfully Added!");
-                                }
-
-                                break;
-
-                            case 2:
-                                if (customer.Count != 0)
-                                {
-                                    foreach (var customers in customer)
-                                    {
-                                        Console.WriteLine(customers.summary());
-                                    }
-                                }
-                                else
-                                {
-                                    Console.WriteLine("There is no Customer found");
-                                }
-
-                                break;
-                            case 3:
-                                Console.WriteLine("Enter Customer ID for deleting:");
-
-                                int searchId = Int32.Parse(Console.ReadLine());
-
-                                Customers deleteCustomer = null;
-                                foreach (Customers customers in customer)
-                                {
-                                    if (customers.customer_id == searchId)
-                                    {
-                                        deleteCustomer = customers;
-                                        break;
-                                    }
-                                }
-
-                                if (deleteCustomer != null)
-                                {
-                                        Console.WriteLine(deleteCustomer.removedCustumers());
-                                        customer.Remove(deleteCustomer);
-                                }
-                                else
-                                {
-                                    Console.WriteLine($"Customer ID ({searchId},) does not exist");
-                                }
-
-                                break;
-
-                            case 4:
-                                backToMainMenu = false;
-                                Console.Clear();
-                                break;
+                            customerPhone = Int64.Parse(Console.ReadLine());
+                            Console.Clear();
+                            break;
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine("Please Enter a number");
                         }
                     }
+                    
+                    Random rndm = new Random();
+                    int customer_id = rndm.Next(1000, 9999);
+                    bool isSameId;
+
+                    bool isSameCustomer = false;
+
+                    foreach (var customers in customer)
+                    {
+                        if (customers.customerEmail == customerEmail || customers.customerPhone == customerPhone)
+                        {
+                            isSameCustomer = true;
+                            break;
+                        }
+                    }
+
+                    if (isSameCustomer)
+                    {
+                        Console.WriteLine(
+                            $"This E-Mail ({customerEmail}) or Phone Number ({customerPhone}) already in the system!");
+                    }
+                    else
+                    {
+                        Customers newCustomer = new Customers(customerName, customerEmail, customerPhone, customer_id);
+                        customer.Add(newCustomer);
+                        Console.WriteLine("Successfully Added!");
+                    }
+
+                    do
+                    {
+                        isSameId = true;
+                        customer_id = rndm.Next(1000, 9999);
+
+                        foreach (var c in customer)
+                        {
+                            if (c.customer_id == customer_id)
+                            {
+                                isSameId = true;
+                                break;
+                            }
+                        }
+                    } while (isSameId);
+
+                    if (isSameId)
+                    {
+                        
+                    }
+                    else
+                    {
+                        Customers newCustomer = new Customers(customerName, customerEmail, customerPhone, customer_id);
+                        customer.Add(newCustomer);
+                    }
+                    break;
+
+                case 2:
+                    if (customer.Count != 0)
+                    {
+                        foreach (var customers in customer)
+                        {
+                            Console.WriteLine(customers.summary());
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("There is no Customer found");
+                    }
+
+                    break;
+                case 3:
+                    Console.WriteLine("Enter Customer ID for deleting:");
+
+
+                    int searchId = Int32.Parse(Console.ReadLine());
+                    Customers deleteCustomer = null;
+                    foreach (Customers customers in customer)
+                    {
+                        if (customers.customer_id == searchId)
+                        {
+                            deleteCustomer = customers;
+                            break;
+                        }
+                    }
+
+                    if (deleteCustomer != null)
+                    {
+                        Console.WriteLine(deleteCustomer.removedCustumers());
+                        customer.Remove(deleteCustomer);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Customer ID ({searchId},) does not exist");
+                    }
+
+                    break;
+
+                case 4:
+                    backToMainMenu = false;
+                    Console.Clear();
+                    break;
+            }
+        }
     }
 }
